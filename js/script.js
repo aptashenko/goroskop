e542f4c974ce908339a71afeddefe6ad83f64280
Artem <aptashenko2019@gmail.com>
2022-04-28 03:40:39 +0300

    готова оболочка

--- js/script.js
+++ js/script.js
@@ -5,50 +5,61 @@ const labelElems = document.querySelectorAll('.quiz__label');
 const buttonNext = document.querySelector('.quiz__button.isActive');
 const birthForm = document.querySelector('.quiz__answer--date');
 const resultBtn = document.querySelector('birthButton');
+const finishBox = document.querySelector('.finish');
 const birthButton = document.createElement('button');
+const footerEl = document.querySelector('.footer');
 birthButton.classList.add('quiz__button', 'birthButton');
 birthButton.setAttribute('type', 'button');
 birthButton.innerText = 'ДАЛЕЕ';
 birthButton.style.display = 'block';
-const finishBox = document.querySelector('.finish');
+
+const footerPosition = document.body.clientHeight;
+footerEl.style.top = `${footerPosition+50}px`;
 
 
 let count = 0;
 
-console.log(birthButton);
-
 radioElems.forEach(el => el.addEventListener('input', color));
+
+buttonNext.addEventListener('click', function () {
+    const activeLabel = document.querySelector('.quiz__label--active');
+    const firstQuiz = quizForm.firstElementChild;
+    const buttonEl = document.querySelector('.quiz__button');
+
+    count += 1;
+    nextQuiz();
+    if (!firstQuiz.classList.contains('isActive')) {
+        mainEl.style.display = "none";
+    }
+
+    if (activeLabel) {
+        activeLabel.classList.remove('quiz__label--active');
+    }
+
+    buttonEl.style.display = 'none';
+});
+
+birthForm.addEventListener('input', getZodiacSign);
+
+birthButton.addEventListener('click', dataProcessing);
 
 function color(evt) {
     const checkedRadio = document.querySelector('.quiz__label--active');
     const buttonEl = document.querySelector('.quiz__button');
+    const checkedLabel = evt.target.parentNode;
+    
+
     if (checkedRadio) {
         checkedRadio.classList.remove('quiz__label--active');
     }
 
-    evt.target.parentNode.classList.add('quiz__label--active');
+    checkedLabel.classList.add('quiz__label--active');
 
-    if (evt.target.parentNode.classList.contains('quiz__label--active')) {
+    if (checkedLabel.classList.contains('quiz__label--active')) {
         buttonEl.style.display = 'block';
+        footerEl.style.top = `${footerPosition + 80}px`;
     }
 }
-
-buttonNext.addEventListener('click', function () {
-    count += 1;
-    nextQuiz();
-    if (!quizForm.firstElementChild.classList.contains('isActive')) {
-        mainEl.style.display = "none";
-    }
-
-    const activeLabel = document.querySelector('.quiz__label--active');
-
-    if (activeLabel) {
-        activeLabel.classList.remove('quiz__label--active');
-    }    
-
-    const buttonEl = document.querySelector('.quiz__button');
-    buttonEl.style.display = 'none';
-});
 
 function nextQuiz() {
     const quizBlocks = document.querySelectorAll('.quiz__question');
@@ -60,149 +71,274 @@ function nextQuiz() {
     });
 }
 
-birthForm.addEventListener('input', getBirthInfo);
+function getZodiacSign() {
 
-function getBirthInfo() {
-    const dayInput = document.querySelector('.quiz__days');
-    const monthInput = document.querySelector('.quiz__monthes');
-    const yearInput = document.querySelector('.quiz__years');
-    const warningEl = document.querySelector('.quiz__warning');
-
-    const sings = {
-        aries: document.querySelector('.quiz__zodiac--aries'),
-        aqua: document.querySelector('.quiz__zodiac--aqua'),
-        cancer: document.querySelector('.quiz__zodiac--cancer'),
-        capricorn: document.querySelector('.quiz__zodiac--capricorn'),
-        gemini: document.querySelector('.quiz__zodiac--gemini'),
-        lion: document.querySelector('.quiz__zodiac--lion'),
-        libra: document.querySelector('.quiz__zodiac--libra'),
-        sagitt: document.querySelector('.quiz__zodiac--sagitt'),
-        scorp: document.querySelector('.quiz__zodiac--scorp'),
-        taurus: document.querySelector('.quiz__zodiac--taurus'),
-        virgo: document.querySelector('.quiz__zodiac--virgo'),
-        prisces: document.querySelector('.quiz__zodiac--prisces'),
+    const elems = {
+        dayInput: document.querySelector('.quiz__days'),
+        monthInput: document.querySelector('.quiz__monthes'),
+        yearInput: document.querySelector('.quiz__years'),
+        warningEl: document.querySelector('.quiz__warning'),
+        zodiacEl: document.querySelectorAll('.quiz__zodiac'),
+        selectBox: document.querySelector('.quiz__answer--date'),
     };
 
-    function addZero(num) {
-        if (num <= 9) {
-            num = '0' + num;
+    const day = Number(elems.dayInput.value);
+    const month = Number(elems.monthInput.value);
+
+    if (month == 1) {
+        if (day <= 19) {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Козерог')) {
+                    el.classList.add('isActive');
+                }
+            });
+        } else {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Водолей')) {
+                    el.classList.add('isActive');
+                }
+            });
         }
-        return num;
+    } else if (month == 2) {
+        if (day <= 18) {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Водолей')) {
+                    el.classList.add('isActive');
+                }
+            });
+        } else {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Рыбы')) {
+                    el.classList.add('isActive');
+                }
+            });
+        }
+    } else if (month == 3) {
+        if (day <= 20) {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Рыбы')) {
+                    el.classList.add('isActive');
+                }
+            });
+        } else {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Овен')) {
+                    el.classList.add('isActive');
+                }
+            });
+        }
+    } else if (month == 4) {
+        if (day <= 19) {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Овен')) {
+                    el.classList.add('isActive');
+                }
+            });
+        } else {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Телец')) {
+                    el.classList.add('isActive');
+                }
+            });
+        }
+    } else if (month == 5) {
+        if (day <= 20) {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Телец')) {
+                    el.classList.add('isActive');
+                }
+            });
+        } else {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Близнецы')) {
+                    el.classList.add('isActive');
+                }
+            });
+        }
+    } else if (month == 6) {
+        if (day <= 21) {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Близнецы')) {
+                    el.classList.add('isActive');
+                }
+            });
+        } else {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Рак')) {
+                    el.classList.add('isActive');
+                }
+            });
+        }
+    } else if (month == 7) {
+        if (day <= 22) {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Рак')) {
+                    el.classList.add('isActive');
+                }
+            });
+        } else {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Лев')) {
+                    el.classList.add('isActive');
+                }
+            });
+        }
+    } else if (month == 8) {
+        if (day <= 22) {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Лев')) {
+                    el.classList.add('isActive');
+                }
+            });
+        } else {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Дева')) {
+                    el.classList.add('isActive');
+                }
+            });
+        }
+    } else if (month == 9) {
+        if (day <= 22) {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Дева')) {
+                    el.classList.add('isActive');
+                }
+            });
+        } else {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Весы')) {
+                    el.classList.add('isActive');
+                }
+            });
+        }
+    } else if (month == 10) {
+        if (day <= 23) {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Весы')) {
+                    el.classList.add('isActive');
+                }
+            });
+        } else {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Скорпион')) {
+                    el.classList.add('isActive');
+                }
+            });
+        }
+    } else if (month == 11) {
+        if (day <= 21) {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Скорпион')) {
+                    el.classList.add('isActive');
+                }
+            });
+        } else {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Стрелец')) {
+                    el.classList.add('isActive');
+                }
+            });
+        }
+    } else if (month == 12) {
+        if (day <= 21) {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Стрелец')) {
+                    el.classList.add('isActive');
+                }
+            });
+        } else {
+            elems.zodiacEl.forEach(el => {
+                if (el.classList.contains('isActive')) {
+                    el.classList.remove('isActive');
+                } if (el.innerHTML.includes('Козерог')) {
+                    el.classList.add('isActive');
+                }
+            });
+        }
     }
 
-    const day = Number(dayInput.value);
-    const month = Number(monthInput.value);
-
-    const birthDate = addZero(month) + '-' + addZero(day);
-
-    if (birthDate >= '01-01' && birthDate <= '01-19') {
-        sings.capricorn.style.display = 'block';
-    } else {
-        sings.capricorn.style.display = 'none';
+    if (elems.dayInput.value ==='null' || elems.monthInput.value ==='null') {
+        elems.zodiacEl.forEach(el => {
+            el.classList.remove('isActive');
+        });
     }
 
-    if (birthDate >= '01-20' && birthDate <= '02-18') {
-        sings.aqua.style.display = 'block';
-    } else {
-        sings.aqua.style.display = 'none';
-    }
-
-    if (birthDate >= '02-19' && birthDate <= '03-20') {
-        sings.prisces.style.display = 'block';
-    } else {
-        sings.prisces.style.display = 'none';
-    }
-
-    if (birthDate >= '03-21' && birthDate <= '04-19') {
-        sings.aries.style.display = 'block';
-    } else {
-        sings.aries.style.display = 'none';
-    }
-
-    if (birthDate >= '04-20' && birthDate <= '05-20') {
-        sings.taurus.style.display = 'block';
-    } else {
-        sings.taurus.style.display = 'none';
-    }
-
-    if (birthDate >= '05-21' && birthDate <= '06-21') {
-        sings.gemini.style.display = 'block';
-    } else {
-        sings.gemini.style.display = 'none';
-    }
-
-    if (birthDate >= '06-22' && birthDate <= '07-22') {
-        sings.cancer.style.display = 'block';
-    } else {
-        sings.cancer.style.display = 'none';
-    }
-
-    if (birthDate >= '07-23' && birthDate <= '08-22') {
-        sings.lion.style.display = 'block';
-    } else {
-        sings.lion.style.display = 'none';
-    }
-
-    if (birthDate >= '08-23' && birthDate <= '09-22') {
-        sings.virgo.style.display = 'block';
-    } else {
-        sings.virgo.style.display = 'none';
-    }
-
-    if (birthDate >= '09-23' && birthDate <= '10-23') {
-        sings.libra.style.display = 'block';
-    } else {
-        sings.libra.style.display = 'none';
-    }
-
-    if (birthDate >= '10-24' && birthDate <= '11-21') {
-        sings.scorp.style.display = 'block';
-    } else {
-        sings.scorp.style.display = 'none';
-    }
-
-    if (birthDate >= '11-22' && birthDate <= '12-21') {
-        sings.sagitt.style.display = 'block';
-    } else {
-        sings.sagitt.style.display = 'none';
-    }
-
-    if (birthDate >= '12-22' && birthDate <= '12-31') {
-        sings.capricorn.style.display = 'block';
-    } else {
-        sings.capricorn.style.display = 'none';
-    }
-
-    if (dayInput.value !== 'null' || monthInput.value !== 'null' || yearInput.value !== 'null') {
-        warningEl.style.display = 'block';
+    if (elems.dayInput.value !== 'null' || elems.monthInput.value !== 'null' || elems.yearInput.value !== 'null') {
+        elems.warningEl.style.display = 'block';
     }
     
-    const selectBox = document.querySelector('.quiz__answer--date');
-
-    if (dayInput.value !== 'null' && monthInput.value !== 'null' && yearInput.value !== 'null') {
-        dayInput.style.color = '#315DFA';
-        monthInput.style.color = '#315DFA';
-        yearInput.style.color = '#315DFA';
-        dayInput.style.fontWeight = '700';
-        monthInput.style.fontWeight = '700';
-        yearInput.style.fontWeight = '700';
-        warningEl.style.display = 'none';
-        selectBox.append(birthButton);
+    if (elems.dayInput.value !== 'null' && elems.monthInput.value !== 'null' && elems.yearInput.value !== 'null') {
+        elems.dayInput.style.color = '#315DFA';
+        elems.monthInput.style.color = '#315DFA';
+        elems.yearInput.style.color = '#315DFA';
+        elems.dayInput.style.fontWeight = '700';
+        elems.monthInput.style.fontWeight = '700';
+        elems.yearInput.style.fontWeight = '700';
+        elems.warningEl.style.display = 'none';
+        elems.selectBox.append(birthButton);
     }
 }
 
-birthButton.addEventListener('click', showResult);
-
-function showResult(e) {
-    e.target.parentNode.parentNode.classList.remove('isActive');
+function dataProcessing(e) {
+    const quiz = e.target.parentNode.parentNode;
+    
+    quiz.classList.remove('isActive');
     finishBox.style.display = 'block';
+
     progress();
-    setTimeout(getResult, 12000);
+    setTimeout(showResult, 12000);
 }
 
 function progress() {
     const progressLine = document.querySelector('.finish__progress--green');
     const progressText = document.querySelectorAll('.finish__item');
+    const doneText = document.querySelectorAll('.finish__item--result');
     
     let width = 1;
     let id = setInterval(progressStatus, 100);
@@ -217,13 +353,16 @@ function progress() {
     }
     
     for (let i = 0; i <= progressText.length; i += 1) {
-    setTimeout(() => progressText[i].style.display='inline-block', 400 * (i + 1));
+        
+        setTimeout(() => progressText[i].classList.add('isActive'), 400 * (i + 1));
     }
+
+    doneText.forEach(el => el.style.display = 'inline');
 
     setTimeout(() => document.querySelector('.finish__text').style.display = 'inline-block', 10000);
 }
 
-function getResult() {
+function showResult() {
     const lastPage = document.querySelectorAll('.finish__result');
     lastPage.forEach(el => {
         if (el.classList.contains('isActive')) {
